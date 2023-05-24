a7b0c73492228723b97df3a2a7a2f6972237543f
Routis Babis <haralampos.routis@netcompany-intrasoft.com>
2023-05-24 21:40:43 +0300

    Added buildscan

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,18 @@
 plugins {
     // Apply the foojay-resolver plugin to allow automatic download of JDKs
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
+
+    id("com.gradle.enterprise") version ("3.9")
+}
+
+gradleEnterprise {
+ if (System.getenv("CI") != null) {
+  buildScan {
+   publishAlways()
+   termsOfServiceUrl = "https://gradle.com/terms-of-service"
+   termsOfServiceAgree = "yes"
+  }
+ }
 }
 
 rootProject.name = "test-pub"
