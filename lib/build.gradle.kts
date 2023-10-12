68fdc83e7d36ba1e9c282cae6cebf625b466e8ff
Dimitris ZARRAS <Dimitris.ZARRAS@netcompany.com>
2023-10-12 17:30:52 +0300

    Add configuration for com.vanniktech.maven.publish and remove release-drafter.

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -2,17 +2,13 @@ import org.jetbrains.dokka.DokkaConfiguration.Visibility
 import org.jetbrains.dokka.gradle.DokkaTask
 import java.net.URL
 
-extra["isReleaseVersion"] = !version.toString().endsWith("SNAPSHOT")
-
 plugins {
-    // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
+    base
+    `java-library`
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.spotless)
     alias(libs.plugins.dokka)
-
-    `java-library`
-    `maven-publish`
-    signing
+    alias(libs.plugins.maven.publish)
 }
 
 repositories {
@@ -42,7 +38,6 @@ testing {
 // Apply a specific Java toolchain to ease working on different environments.
 java {
     sourceCompatibility = JavaVersion.toVersion(libs.versions.java.get())
-    withSourcesJar()
 }
 
 kotlin {
@@ -62,15 +57,6 @@ spotless {
 }
 
 //
-// Redefine javadocJar in terms of Dokka
-//
-val javadocJar = tasks.register<Jar>("javadocJar") {
-    dependsOn(tasks.dokkaHtml)
-    from(tasks.dokkaHtml.flatMap { it.outputDirectory })
-    archiveClassifier.set("javadoc")
-}
-
-//
 // Configuration of Dokka engine
 //
 tasks.withType<DokkaTask>().configureEach {
@@ -78,7 +64,10 @@ tasks.withType<DokkaTask>().configureEach {
         named("main") {
             documentedVisibilities.set(setOf(Visibility.PUBLIC, Visibility.PROTECTED))
 
-            val remoteSourceUrl = System.getenv()["GIT_REF_NAME"]?.let { URL("https://github.com/routis/test-pub/tree/$it/lib/src") }
+            val remoteSourceUrl = System.getenv()["GIT_REF_NAME"]
+                ?.let {
+                    URL("https://github.com/routis/test-pub/tree/$it/lib/src")
+                }
             remoteSourceUrl
                 ?.let {
                     sourceLink {
@@ -89,57 +78,4 @@ tasks.withType<DokkaTask>().configureEach {
                 }
         }
     }
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("library") {
-            from(components["java"])
-            artifacts + artifact(javadocJar)
-            pom {
-                name.set("Test Lib")
-                description.set("Playground for gradle and github actions")
-                url.set("https://github.com/routis/test-pub")
-                licenses {
-                    license {
-                        name.set("The Apache License, Version 2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-                scm {
-                    connection.set("scm:git:git://git@github.com:routis/test-pub.git")
-                    developerConnection.set("scm:git:ssh://git@github.com:routis/test-pub.git")
-                    url.set("https://github.com/routis/test-pub")
-                }
-                developers {
-                    developer {
-                        organizationUrl.set("https://github.com/routis/")
-                    }
-                }
-            }
-        }
-    }
-
-    repositories {
-        val sonaUri =
-            if ((extra["isReleaseVersion"]) as Boolean) {
-                "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
-            } else {
-                "https://s01.oss.sonatype.org/content/repositories/snapshots/"
-            }
-
-        maven {
-            name = "sonatype"
-            url = uri(sonaUri)
-            credentials(PasswordCredentials::class)
-        }
-    }
-}
-
-signing {
-    val signingKeyId: String? by project
-    val signingKey: String? by project
-    val signingPassword: String? by project
-    useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
-    sign(publishing.publications["library"])
 }
